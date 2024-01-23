182bfadf7d19347855cd82c2cc46f6a98d5fdd45
Matias Sombeck <matiasgent@gmail.com>
2024-01-23 02:07:43 -0300

    se corrige parpadeo de numero y nombre de canal, y boton contacto

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,12 +5,20 @@ import './Header.css'
 function Header(){
 
    const [verMail,setVerMail] = useState(null);
+   const [mailClick,setMailClick] = useState(false);
+   const showMail = () =>{
+    verMail==null?setVerMail('/public/mailImg.png'):setVerMail(null);
+    mailClick?setMailClick(false):setMailClick(true);
+   }
 
    const mail = "contact@sombeck.dev"
-
    const copyToClip = () =>{
     navigator.clipboard.writeText(mail)
    }
+
+   
+
+
     return(
         <>
         <header>
@@ -20,10 +28,14 @@ function Header(){
                 <li><a target='_blanck' rel="noopener" href="https://github.com/matiassom" ><img className="icono" title="githubIco" id="githubIco" src="/public/icons8-github-24.png"/>Github</a></li>
                 <li><a target='_blanck' rel="noopener" href="https://www.linkedin.com/in/matias-s-6b6b41271/" ><img title="linkedinIco" id="linkedinIco" className="icono" src="/public/icons8-linkedin-50.png"/>Linkedin</a></li>
                 <li><img title="IcoEnglish" id="usFlagIco" src="/public/estados-unidos-de-america.png"/>English</li>
-                <li onClick={()=>{verMail==null?setVerMail('/public/mailImg.png'):setVerMail(null)}}><img title="IcoMail" id="mailIco" src="/public/icons8-correo-de-manzana-50.png"/>Contacto</li>
+                {mailClick?
+                <li onClick={showMail} id='liContacto'><img title="IcoMail" id="mailIco" src="/public/icons8-correo-de-manzana-50.png"/>Contacto</li>
+                :
+                <li onClick={showMail} ><img title="IcoMail" id="mailIco" src="/public/icons8-correo-de-manzana-50.png"/>Contacto</li>
+                }
             </ul>
             {
-            verMail&&(console.log("se ejecuta"),
+            verMail&&(
                 <>
                 <div id='imgMail'>
                     <img id='dirMail' src={verMail}/>
